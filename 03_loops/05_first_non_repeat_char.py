ff9198c91715c1781b2a053baad222d2eea0610c
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2026-04-21 11:44:01 +0530

    method 2 , using count function

--- 03_loops/05_first_non_repeat_char.py
+++ 03_loops/05_first_non_repeat_char.py
@@ -2,14 +2,19 @@
 str1 = input("Enter any string ::  ")
 #sdfkjigdfkfjs -> i and g non repeat , i is 1st -> answer i
 n = len(str1)
-for i in range(0,n):
-  isRepeated = False
-  for j in range(i+1,n):
-    if(str1[i]==str1[j]):
-      isRepeated = True
-      break
-  if isRepeated==False:
-     print("First Repeated character is :: ",str1[i])
-     exit()
+# for i in range(0,n):
+#   isRepeated = False
+#   for j in range(i+1,n):
+#     if(str1[i]==str1[j]):
+#       isRepeated = True
+#       break
+#   if isRepeated==False:
+#      print("First Repeated character is :: ",str1[i])
+#      exit()
 
-print("NA")
+# print("NA")
+
+for ch in str1:
+  if str1.count(ch)==1:
+    print("First Repeated Character is :: ",ch)
+    break
