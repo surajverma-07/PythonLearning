3c794acb0bfdda4daa260e7dabbd0f23eb585e8d
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2026-04-21 11:42:10 +0530

    findin first non repeat character

---
+++ 03_loops/05_first_non_repeat_char.py
@@ -0,0 +1,15 @@
+# find the first non repeated character in a string 
+str1 = input("Enter any string ::  ")
+#sdfkjigdfkfjs -> i and g non repeat , i is 1st -> answer i
+n = len(str1)
+for i in range(0,n):
+  isRepeated = False
+  for j in range(i+1,n):
+    if(str1[i]==str1[j]):
+      isRepeated = True
+      break
+  if isRepeated==False:
+     print("First Repeated character is :: ",str1[i])
+     exit()
+
+print("NA")
