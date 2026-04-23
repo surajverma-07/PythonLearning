db884c7c82fc1b91e18e1e823a0d9733ef9c93f6
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2026-04-23 18:51:30 +0530

    Encapsulation learned

--- 07_oops/01_car_classes.py
+++ 07_oops/01_car_classes.py
@@ -1,11 +1,14 @@
 class Car:
    def __init__(self, brand,model):
-      self.brand = brand
+      self.__brand = brand
+      # with __variable_name is private
       self.model = model
    def full_name(self):
       # return self.brand + " "+ self.model
-      return f"{self.brand} {self.model}"
-
+      return f"{self.__brand} {self.model}"
+   def get_brand(self):
+       return self.__brand
+   
 class ElectricCar(Car):
       def __init__(self,brand,model,battery_size):
           super().__init__(brand,model)
@@ -13,12 +16,13 @@ class ElectricCar(Car):
 
 
 my_ecar = ElectricCar("Telsla","Model S","85kWh")
-print("My E Car details :: ",my_ecar.brand," ",my_ecar.model," ",my_ecar.battery_size)
-
+# print("My E Car details :: ",my_ecar.brand," ",my_ecar.model," ",my_ecar.battery_size)
+# print(my_ecar.__brand)
+print(my_ecar.get_brand())
 my_car = Car("Toyota","Fortuner")
 # print(my_car.brand)
 # print(my_car.model)
-print(my_car.full_name())
+# print(my_car.full_name())
 
 my_new_car = Car("Tata","Punch")
-print(my_new_car.model)
+# print(my_new_car.model)
