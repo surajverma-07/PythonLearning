83bb2d181eb5675de7bd044681949cf78a628cf7
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2026-04-23 19:41:08 +0530

    Multiple inheritence

--- 07_oops/01_car_classes.py
+++ 07_oops/01_car_classes.py
@@ -4,10 +4,10 @@ class Car:
       self.__brand = brand
       # with __variable_name is private
       Car.car_count += 1
-      self.model = model
+      self.__model = model
    def full_name(self):
       # return self.brand + " "+ self.model
-      return  f"{self.__brand} {self.model}"
+      return  f"{self.__brand} {self.__model}"
    def get_brand(self):
        return self.__brand
    def fuel_type(self):
@@ -15,6 +15,10 @@ class Car:
    @staticmethod
    def general_desc():
        return "Static method it is "
+   # property used to make the variable readable only and we can access the fn as a property 
+   @property
+   def model(self):
+       return self.__model
        
 class ElectricCar(Car):
       def __init__(self,brand,model,battery_size):
@@ -38,6 +42,27 @@ my_car = Car("Toyota","Fortuner")
 my_new_car = Car("Tata","Punch")
 # print(my_new_car.model)
 # print(my_new_car.fuel_type())
+# my_new_car.model = "Nexon"
+print(my_new_car.model)
 
 # print("Total Cars :: ",Car.car_count)
-print(Car.general_desc())
+# print(Car.general_desc())
+
+# print(isinstance(my_ecar,ElectricCar))
+# print(isinstance(my_ecar,Car))
+# print(isinstance(my_car,Car))
+# print(isinstance(my_car,ElectricCar))
+
+class Battery:
+    def battery_info(self):
+        return "this is battery"
+class Engine:
+    def engine_info(self):
+        return "this is engine"
+
+class EvCar(Battery,Engine,Car):
+    pass
+
+my_ev = EvCar("tata ev","harrier ev")
+print(my_ev.engine_info())
+print(my_ev.battery_info())
